cb209db2e0d0a2604f7f4f522b286c775eccd074
memory-Ieak <dimitri.escaffre-faure@epita.fr>
2023-12-16 21:37:25 +0100

    add files

---
+++ config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Vérifier si Python 3 est installé
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 n'est pas installé. Veuillez installer Python 3 avant de continuer."
+    exit 1
+fi
+
+# Installer les bibliothèques Python nécessaires
+python3 -m pip install --upgrade pip
+python3 -m pip install numpy matplotlib scikit-learn
+
+echo "Les bibliothèques ont été installées avec succès."
